74ae8187bb3e12e60dbe475aa3ea59df4ffc5cfd
Stanislav <verbatim8872@gmail.com>
2021-07-02 21:53:53 +0300

    add mutations

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema');
+
+const users = [
+    { id: 1, username: 'Valya', age: 25 }
+];
+
+const createUser = (input) => {
+    const id = Date.now();
+    return { id, ...input };
+}
+
+const root = { 
+    getAllUsers: () => users,
+    getUser: ({ id }) => users.find(user => user.id === id),
+    createUser: ({ input }) => {
+        const user = createUser(input);
+        console.log(user);
+        users.push(user);
+        return user;
+    }
+};
 
 const app = express();
 
 app.use(cors());
 app.use('/graphql', graphqlHTTP({
-    graphql: true
-}))
+    schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
 
 app.listen(5000, () => console.log(`SEVER STARTED at http://localhost:5000`));
